7f3b2ed833a8a30c320822c41d4db63cd6b37ae2
Al-amin Sahed <alaminsahed97@gmail.com>
2021-06-19 09:51:58 +0600

    home-done

--- src/Component/HeaderMain.js
+++ src/Component/HeaderMain.js
@@ -1,28 +1,49 @@
 import React from "react";
- import { useEffect, useRef } from "react";
 import "./CSS/HeaderMain.css";
-// import $ from "jquery";
-// import banner from '../image/background.png';
-// import WaterWave from "react-water-wave";
+import { Parallax } from "react-parallax";
+import banner from "../image/background.png";
+import Fade from "react-reveal/Fade";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(fas, fab);
 
 const HeaderMain = () => {
-//   const waterEffect = useRef();
-
-  useEffect(() => {
-    // const $el = $(waterEffect.current);
-    // //console.log($el)
-    // $el.ripples();
-  }, []);
-
-// <WaterWave
-// imageUrl={banner}
-// >
-
-// </WaterWave>
-
   return (
     <div className="header" id="header">
-    <h1>header</h1>
+      <Parallax
+        className="header"
+        blur={5}
+        bgImage={banner}
+        bgImageAlt="banner"
+        strength={500}
+      >
+        <div className="banner-content">
+          <Fade top>
+            <div className="fadeInUp">
+              <h5>HELLO</h5>
+              <h2>I am Md.Al-amin Sahed</h2>
+              <h4>Front-end Developer</h4>
+            </div>
+          </Fade>
+          <div className="social-media">
+            <a href="https://www.linkedin.com/in/md-al-amin-sahed/">
+              <FontAwesomeIcon icon={["fab", "linkedin"]} className="icon" />
+            </a>
+            <a href="https://github.com/alaminsahed">
+              <FontAwesomeIcon icon={["fab", "github"]}  className="icon" />
+            </a>
+            <a href="https://alaminsahed.medium.com/">
+              <FontAwesomeIcon icon={["fab", "medium"]}  className="icon" />
+            </a>
+            <a href="mailto:alaminsahed101@gmail.com">
+              <FontAwesomeIcon icon={["fas", "envelope"]} className="icon" />
+            </a>
+          </div>
+        </div>
+      </Parallax>
     </div>
   );
 };
