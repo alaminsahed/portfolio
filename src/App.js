aeeb471d37fa6a15a345e4ebc7a1d42e7a1ec9af
alaminsahed <alaminsahed97@gmail.com>
2021-06-25 22:31:24 +0600

    test 01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './Pages/Home';
 import "./App.css";
+import About from './Component/About';
 
 const App = () => {
   return (
     <div className="app">
       <Home/>
+      <About/>
     </div>
   );
 };
