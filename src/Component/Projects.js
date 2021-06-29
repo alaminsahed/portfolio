238b11b1809bd3704580c8452e117e3077fd7df9
alaminsahed <alaminsahed97@gmail.com>
2021-06-29 12:10:20 +0600

    portfolio bar prbl solve

--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React from "react";
 import "./CSS/Projects.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col,Tabs, Tab } from "react-bootstrap";
 import Fade from "react-reveal/Fade";
 
 const Projects = () => {
-    return (
-        <div className="projects" id="projects">
-          <Container>
-          <div className="skill-title">
+  return (
+    <div className="projects" id="projects">
+      <Container>
+        <div className="skill-title">
           <Fade top>
-            <h2 className="t-color">My Stack</h2>
-            <p className="skill-text">
-              Always open to learn new technologies and skills
-            </p>
+            <h2 className="t-color">My Portfolio</h2>
+            <p className="skill-text">Working on a Project is a great fun</p>
           </Fade>
           <Fade left>
             <span className="bottom-line"></span>
           </Fade>
         </div>
+        <div className="project-body">
+         <div className="project-tab ">
+         <Tabs defaultActiveKey="profile" variant="pills" className="justify-content-center">
+            <Tab eventKey="home" title="Home">
+            
+            </Tab>
+            <Tab eventKey="profile" title="Profile">
+             
+            </Tab>
+            <Tab eventKey="contact" title="Contact">
+            
+            </Tab>
+          </Tabs>
 
-          </Container>
+         </div>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default Projects;
