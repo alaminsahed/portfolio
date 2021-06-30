44a0e2c8e190c96cdecef5a67ed63dd59edb274b
alaminsahed <alaminsahed97@gmail.com>
2021-07-01 01:18:44 +0600

    blog structure

--- src/Component/Blog.js
+++ src/Component/Blog.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React from "react";
 import "./CSS/Blog.css";
+import Fade from "react-reveal/Fade";
+import { Container, Row, Col, Tabs, Tab, Card } from "react-bootstrap";
+import Blogdata from "../Data/Blogdata/BlogData";
+import BlogDetails from "./BlogDetails";
 
 const Blog = () => {
-    return (
-        <div className="blog" id="blog">
-            <h1>blog</h1>
-        </div>
-    );
+  return (
+    <div className="blog" id="blog">
+        <Container>
+      <div className="skill-title">
+        <Fade top>
+          <h2 className="t-color">Blogs</h2>
+          <p className="skill-text">Blog helps to clear a concept</p>
+        </Fade>
+        <Fade left>
+          <span className="bottom-line"></span>
+        </Fade>
+      </div>
+      <div className="blog-body">
+        {
+            Blogdata.map(info=><BlogDetails key={info.id} info={info}/>)
+        }
+
+      </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Blog;
