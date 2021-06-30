4b1aaf3f1bd4b3358ce35f314f95020e5ac0a5dd
alaminsahed <alaminsahed97@gmail.com>
2021-06-30 23:33:46 +0600

    project done

--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -4,6 +4,12 @@ import { Container, Row, Col, Tabs, Tab, Card } from "react-bootstrap";
 import Fade from "react-reveal/Fade";
 import data from "../Data/projectData";
 import ProjectDetails from "./PojectDetails";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, fas);
 
 const Projects = () => {
   return (
@@ -44,20 +50,58 @@ const Projects = () => {
                       </Col>
                     ))}
                   <Col md={4}>
-                    <Card>
-                      <Card.Img variant="top" src="holder.js/100px180" />
-                      <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                          Some quick example text to build on the card title and
-                          make up the bulk of the card's content.
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
+                    <div className="more-card">
+                      <Card>
+                        <Card.Body className="more-body">
+                          <Card.Title>More</Card.Title>
+                          <Card.Text>
+                            <a
+                              href="https://github.com/alaminsahed"
+                              alt="github"
+                            >
+                              <FontAwesomeIcon
+                                icon={["fab", "github"]}
+                                size="5x"
+                              />
+                            </a>
+                          </Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </div>
                   </Col>
                 </Row>
               </Tab>
-              <Tab eventKey="fullstack" title="Full-Stack"></Tab>
+              <Tab eventKey="fullstack" title="Full-Stack">
+                <Row>
+                  {data
+                    .filter((info) => info.topic === "fullstack")
+                    .map((project) => (
+                      <Col md={4} key={project.id}>
+                        <ProjectDetails key={project.id} info={project} />
+                      </Col>
+                    ))}
+                  <Col md={4}>
+                    <div className="more-card">
+                      <Card>
+                        <Card.Body className="more-body">
+                          <Card.Title>More</Card.Title>
+                          <Card.Text>
+                            <a
+                              href="https://github.com/alaminsahed"
+                              alt="github"
+                            >
+                              <FontAwesomeIcon
+                                icon={["fab", "github"]}
+                                size="5x"
+                              />
+                            </a>
+                          </Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </div>
+                  </Col>
+                </Row>
+              </Tab>
             </Tabs>
           </div>
         </div>
